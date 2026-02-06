b5179110b609e82039eb50b52d885e8ad801f6a2
sxhw2021 <771690122@qq.com>
2026-02-06 15:24:45 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,14 +46,14 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
 
-    compileOnly(libs.xposed.api)
+    compileOnly("de.robv.android.xposed:api:82")
 
-    ksp(libs.yukihookapi.ksp.xposed)
-    implementation(libs.yukihookapi.api)
+    implementation("com.highcapable.yukihookapi:api:1.3.1")
+    ksp("com.highcapable.yukihookapi:ksp-xposed:1.3.1")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.7.0")
